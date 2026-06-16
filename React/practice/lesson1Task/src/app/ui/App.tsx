ae4aa3d94e2f02190561d71a84acf3c94d8f0f86
Thang <minhthang7209@gmail.com>
2026-06-16 19:33:07 +0300

    add lesson2 code

---
+++ React/practice/lesson1Task/src/app/ui/App.tsx
@@ -0,0 +1,10 @@
+export default function App() {
+    const name: string = 'Ivan';
+    const age: number = 30;
+    const student: boolean = true;
+    return <div>
+        console.log("Name: ", name);
+        console.log("Age: ", age);
+        console.log("Is student: ", student);
+    </div>
+}
