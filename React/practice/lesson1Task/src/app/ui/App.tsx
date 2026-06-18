2d262a31584aa12bb60df29d06ad86ef22a38fa4
Thang <minhthang7209@gmail.com>
2026-06-18 21:47:09 +0300

    add lesson3 code

--- React/practice/lesson1Task/src/app/ui/App.tsx
+++ React/practice/lesson1Task/src/app/ui/App.tsx
@@ -1,10 +1,23 @@
+import { useState } from "react"
+
 export default function App() {
-    const name: string = 'Ivan';
-    const age: number = 30;
-    const student: boolean = true;
+    const [likes, setLikes] = useState(5)
+    const [value, setValue] = useState('Text in input')
+    function increament() {
+        setLikes(likes + 1);
+    }
+    function decrement() {
+        setLikes(likes - 1);
+    }
     return <div>
-        console.log("Name: ", name);
-        console.log("Age: ", age);
-        console.log("Is student: ", student);
+        <h1>{likes}</h1>
+        <h1>{value}</h1>
+        <input
+            type="text"
+            value={value}
+            onChange={event => setValue(event.target.value)}
+        />
+        <button onClick={increament}>Increament</button>
+        <button onClick={decrement}>Decreament</button>
     </div>
 }
